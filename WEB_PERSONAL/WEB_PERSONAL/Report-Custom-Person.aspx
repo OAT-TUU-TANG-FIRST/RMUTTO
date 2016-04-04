fa7019cf1edb3301b77121197aa69640218249c5
Cheezzie O_O <zplaygiirlz1@hotmail.com>
2016-04-04 16:06:37 +0700

    REPORT

---
+++ WEB_PERSONAL/WEB_PERSONAL/Report-Custom-Person.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="Report-Custom-Person.aspx.cs" Inherits="WEB_PERSONAL.Report_Custom_Person" %>
+
+<%@ Register assembly="CrystalDecisions.Web, Version=13.0.2000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" namespace="CrystalDecisions.Web" tagprefix="CR" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Panel ID="Panel1" runat="server">
+        <div>
+            <fieldset>
+                <legend>Test</legend>
+                <table>
+                    <tr>
+                        <td>
+
+                            CITIZEN_ID
+                            <asp:TextBox ID="txt1" runat="server"> </asp:TextBox>
+                            <asp:Button ID="btnShowReport" Text="Show Report" runat="server" onclick="btnShowReport_Click"> </asp:Button>
+                            <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="True" Height="1202px" ReportSourceID="CrystalReportSource1" ToolPanelWidth="200px" Width="1104px" ToolPanelView="None" />
+                       
+                            <CR:CrystalReportSource ID="CrystalReportSource1" runat="server">
+                                <Report FileName="CrystalReport1.rpt">
+                                </Report>
+                            </CR:CrystalReportSource>
+                       
+                        </td>
+                    </tr>
+                </table>
+            </fieldset>
+        </div>
+    </asp:Panel>
+
+
+</asp:Content>
---
+++ WEB_PERSONAL/WEB_PERSONAL/Report-Custom-Person.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using CrystalDecisions.ReportSource;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace WEB_PERSONAL
+{
+    public partial class Report_Custom_Person : System.Web.UI.Page
+    {
+        public static string strConn = @"Data Source = ORCL_RMUTTO;USER ID=RMUTTO;PASSWORD=Zxcvbnm";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CrystalDecisions.CrystalReports.Engine.ReportDocument rpt = new CrystalDecisions.CrystalReports.Engine.ReportDocument();
+            rpt.Load(Server.MapPath("CrystalReport1.rpt"));
+            ConnectionInfo crConnectionInfo = new ConnectionInfo();
+            crConnectionInfo.ServerName = "ORCL_RMUTTO"; //Database server or ODBC
+            //crConnectionInfo.DatabaseName = "ORCL_RMUTTO"; // Database name
+            crConnectionInfo.UserID = "RMUTTO"; // username
+            crConnectionInfo.Password = "Zxcvbnm"; // password
+
+            TableLogOnInfos crTableLogonInfos = new TableLogOnInfos();
+            TableLogOnInfo crTableLogonInfo = new TableLogOnInfo();
+            foreach (CrystalDecisions.CrystalReports.Engine.Table table in rpt.Database.Tables)
+            {
+                crTableLogonInfo.TableName = table.Name;
+                crTableLogonInfo.ConnectionInfo = crConnectionInfo;
+                crTableLogonInfos.Add(crTableLogonInfo);
+                table.ApplyLogOnInfo(crTableLogonInfo);
+            }
+            CrystalReportViewer1.LogOnInfo = crTableLogonInfos;
+            rpt.SetParameterValue("CITIZEN PARAMETER", this.txt1.Text);
+            CrystalReportViewer1.ReportSource = rpt;
+        }
+
+        protected void btnShowReport_Click(object sender, EventArgs e)
+        {
+            
+            ReportDocument rpt = new ReportDocument();
+            rpt.Load(Server.MapPath("CrystalReport1.rpt"));
+            rpt.SetParameterValue("CITIZEN PARAMETER", this.txt1.Text);
+            this.CrystalReportViewer1.ReportSource = rpt;
+        }
+
+    }
+}
